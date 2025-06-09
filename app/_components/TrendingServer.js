d9a31caef429977d3a4215f02cead32d817ce393
Hwezah <hwezah.dev@gmail.com>
2025-06-09 06:46:56 +0300

    minor bug fixes

--- app/_components/TrendingServer.js
+++ app/_components/TrendingServer.js
@@ -1,6 +1,6 @@
-import Podcasts from "../api/podcasts/podcastsApi";
+import Podcasts from "@/app/api/podcasts/podcastsApi";
 import Sermons from "@/app/api/sermons/sermonsApi";
-import Bible from "./Bible";
+// import Bible from "./Bible";
 export default function TrendingServer({ selected, query }) {
   if (selected === "Podcasts") return <Podcasts query={query} />;
   if (selected === "Sermons") return <Sermons query={query} />; // Sermons is the default if no query
--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import AppBody from "./AppBody";
-import Trending from "./_components/TrendingClient"; // or the correct path
+import Trending from "./_components/TrendingClient";
 
 export default function Page({ searchParams }) {
-  return (
-    <AppBody trendingSlot={<Trending searchParams={searchParams} />} /> )
- 
+  return <AppBody trendingSlot={<Trending searchParams={searchParams} />} />;
 }
