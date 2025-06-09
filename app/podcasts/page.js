3c869bd1d32dfab25e2c244a610157d641efc85e
Hwezah <hwezah.dev@gmail.com>
2025-06-09 04:14:15 +0300

    file restructure

--- app/podcasts/page.js
+++
@@ -1,5 +0,0 @@
-import Podcasts from "@/app/_lib/api/podcastsApi";
-
-export default function Page({ searchParams }) {
-  return <Podcasts query={searchParams.query} />;
-}
--- app/sermons/page.js
+++
@@ -1,5 +0,0 @@
-import Sermons from "@/app/_lib/api/sermonsApi";
-
-export default function Page({ searchParams }) {
-  return <Sermons query={searchParams.query} />;
-}
