5269bcd347aa717585d226c4d56f31c2e13f72d0
Hwezah <hwezah.dev@gmail.com>
2025-11-13 01:50:01 -0800

    renaming file

--- app/api/hero/route.js
+++ app/api/hero/route.js
@@ -1,4 +1,3 @@
-// g:\foundation(Client..Next.js Refactor)\app\api\hero-suggestions\route.js
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
+// import nextPlugin from "@next/eslint-plugin-next";
+// import globals from "globals";
+
+// /** @type {import('eslint').Linter.FlatConfig[]} */
+// const eslintConfig = [
+//   {
+//     files: ["**/*.{js,jsx,ts,tsx}"],
+//     plugins: {
+//       "@next/next": nextPlugin,
+//     },
+//     rules: {
+//       ...nextPlugin.configs.recommended.rules,
+//       ...nextPlugin.configs["core-web-vitals"].rules,
+//     },
+//     languageOptions: { globals: { ...globals.browser, ...globals.node } },
+//   },
+// ];
+
+// export default eslintConfig;
 import nextPlugin from "@next/eslint-plugin-next";
 import globals from "globals";
 
@@ -9,8 +28,10 @@ const eslintConfig = [
       "@next/next": nextPlugin,
     },
     rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
+      // Next.js recommends enabling all the rules via plugin
+      "@next/next/no-html-link-for-pages": "warn",
+      "@next/next/no-img-element": "warn",
+      // add any custom overrides here
     },
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
